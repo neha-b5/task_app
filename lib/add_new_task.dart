284c2f843b18ad3cdd62d92c573aa5c5afe25824
Neha B <nehabandlamudi@gmail.com>
2025-07-07 19:57:32 +0530

    Add files via upload

---
+++ lib/add_new_task.dart
@@ -0,0 +1,127 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flex_color_picker/flex_color_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:frontend/utils.dart';
+import 'package:intl/intl.dart';
+
+class AddNewTask extends StatefulWidget {
+  const AddNewTask({super.key});
+
+  @override
+  State<AddNewTask> createState() => _AddNewTaskState();
+}
+
+class _AddNewTaskState extends State<AddNewTask> {
+  final titleController = TextEditingController();
+  final descriptionController = TextEditingController();
+  DateTime selectedDate = DateTime.now();
+  Color _selectedColor = Colors.blue;
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    descriptionController.dispose();
+    super.dispose();
+  }
+
+  Future<void> uploadTaskToDb() async {
+    try {
+      final data = await FirebaseFirestore.instance.collection("tasks").add({
+        "title": titleController.text.trim(),
+        "description": descriptionController.text.trim(),
+        "date": selectedDate,
+        "creator": FirebaseAuth.instance.currentUser!.uid,
+        "postedAt": FieldValue.serverTimestamp(),
+        "color": rgbToHex(_selectedColor),
+      });
+      print(data.id);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add New Task'),
+        actions: [
+          GestureDetector(
+            onTap: () async {
+              final selDate = await showDatePicker(
+                context: context,
+                firstDate: DateTime.now(),
+                lastDate: DateTime.now().add(
+                  const Duration(days: 90),
+                ),
+              );
+              if (selDate != null) {
+                setState(() {
+                  selectedDate = selDate;
+                });
+              }
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                DateFormat('MM-d-y').format(selectedDate),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: titleController,
+                decoration: const InputDecoration(
+                  hintText: 'Title',
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: descriptionController,
+                decoration: const InputDecoration(
+                  hintText: 'Description',
+                ),
+                maxLines: 3,
+              ),
+              const SizedBox(height: 10),
+              ColorPicker(
+                pickersEnabled: const {
+                  ColorPickerType.wheel: true,
+                },
+                color: Colors.blue,
+                onColorChanged: (Color color) {
+                  setState(() {});
+                },
+                heading: const Text('Select color'),
+                subheading: const Text('Select a different shade'),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () async {
+                  await uploadTaskToDb();
+                },
+                child: const Text(
+                  'SUBMIT',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Flutter\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\nehab\\Documents\\ISTE\\task_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Flutter\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\nehab\\Documents\\ISTE\\task_app"
+  "FLUTTER_ROOT=C:\\Flutter\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\nehab\\Documents\\ISTE\\task_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\nehab\\Documents\\ISTE\\task_app"
+  "FLUTTER_TARGET=C:\\Users\\nehab\\Documents\\ISTE\\task_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzIuMg==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049OGRlZmFhNzFhNw==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MTA5MTUwODkzOQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\nehab\\Documents\\ISTE\\task_app\\.dart_tool\\package_config.json"
+)
